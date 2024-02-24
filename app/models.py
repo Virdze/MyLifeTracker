afc41bfd9b7fed98ceb2e85c2e115d6a97674192
João Freitas <64098467+Virdze@users.noreply.github.com>
2024-02-24 10:49:42 +0000

    Added Exercise obj & exercises List to Workout obj

--- app/models.py
+++ app/models.py
@@ -1,10 +1,15 @@
 from mongoengine import Document, StringField, IntField, FloatField, DateTimeField, ListField, ReferenceField
 
-
+class Exercise(Document):
+    name = StringField(required=True)
+    description = StringField()
+    sets = IntField(required=True)
+    reps = IntField(required=True)
 
 class Workout(Document):
     workout_name = StringField(required=True)
     duration = StringField(required=True)
+    exercises = ListField(ReferenceField(Exercise))
     description = StringField()
     date = DateTimeField(required=True)
 
