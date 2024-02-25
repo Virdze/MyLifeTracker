aa33a2c61938073d4106203ec94a9b19c028afc5
João Freitas <64098467+Virdze@users.noreply.github.com>
2024-02-25 12:02:33 +0000

    Added Macro & Micronutrients objects

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from mongoengine import Document, StringField, IntField, FloatField, DateTimeField, ListField, ReferenceField
+from mongoengine import Document, StringField, IntField, FloatField, DateTimeField, ListField, ReferenceField, DictField
 
 class Exercise(Document):
     name = StringField(required=True)
@@ -13,8 +13,26 @@ class Workout(Document):
     description = StringField()
     date = DateTimeField(required=True)
 
+class MacroNutrients(Document):
+    protein = FloatField(required=True)
+    carbohydrates = FloatField(required=True) 
+    fats = FloatField(required=True)
+
+class MicroNutrients(Document):
+    vitamins = DictField(required=True)
+    calcium = FloatField(required=True) 
+    chloride = FloatField(required=True) 
+    magnesium = FloatField(required=True) 
+    phosphorus = FloatField(required=True) 
+    potassium = FloatField(required=True) 
+    sodium = FloatField(required=True)
+
 class Ingredient(Document):
     name = StringField(required=True)
+    weight  = FloatField(required=True)
+    quantity = IntField(required=True)
+    macros = ReferenceField(MacroNutrients)
+    micros = ReferenceField(MicroNutrients)
 
 class Recipe(Document):
     title = StringField(required=True)
